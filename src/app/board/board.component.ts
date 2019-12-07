6755309e9636a3d2e4d0dfef8c080fa4338c2023
alexkaufman06 <alexkaufman06@gmail.com>
2019-12-06 21:26:58 -0800

    Fix typescript warning

--- src/app/board/board.component.ts
+++ src/app/board/board.component.ts
@@ -47,8 +47,8 @@ export class BoardComponent implements OnInit {
       [2, 4, 6]
     ];
 
-    for (let i = 0; i < lines.length; i++) {
-      const [a, b, c] = lines[i];
+    for (const line of lines) {
+      const [a, b, c] = line;
       if (this.squares[a] &&
           this.squares[a] === this.squares[b] &&
           this.squares[a] === this.squares[c]
